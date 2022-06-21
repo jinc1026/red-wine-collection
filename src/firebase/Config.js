3bc8933c163bc76e36107a9068d8ea09ff2caae8
Jin Choe <jinc1026@hotmail.com>
2022-06-21 19:00:41 +0000

    Configure React app for deployment to Github pages

---
+++ src/firebase/Config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBralj4nIoNpd4jVXgbiP610FXgc1W5uAI",
+  authDomain: "red-wine-collection-65671.firebaseapp.com",
+  projectId: "red-wine-collection-65671",
+  storageBucket: "red-wine-collection-65671.appspot.com",
+  messagingSenderId: "404745242358",
+  appId: "1:404745242358:web:f746f3598614f60ea34ed7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
